a6b289ed55c0791bc8c5333cde60042669b11d45
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-09-22 08:44:26 +0300

    add middleware to encode the json data from user

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-
 import express from "express";
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // routes
 import router from "./routes/booksRoute.js";
 
