e7d196bb8edb64c72c2ec5bd2f68707fb1c8365d
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-09-22 09:06:34 +0300

    add update book fn with id

--- controllers/booksControllers.js
+++ controllers/booksControllers.js
@@ -31,7 +31,50 @@ export const addBook = (req, res) => {
   });
 };
 
-export const updateBook = (req, res) => {};
+/*
++   const query = `UPDATE books SET ${fields.join(", ")} WHERE id = ?`;
++   values.push(id);
+*/
+
+export const updateBook = (req, res) => {
+  const { id } = req.params;
+  const { title, description, cover } = req.body;
+
+  const fields = [];
+  const values = [];
+  
+  if (title !== undefined) {
+    fields.push("title = ?");
+    values.push(title);
+  }
+  if (description !== undefined) {
+    fields.push("description = ?");
+    values.push(description);
+  }
+  if (cover !== undefined) {
+    fields.push("cover = ?");
+    values.push(cover);
+  }
+  if (fields.length === 0) {
+    return res.status(400).json({ success: false, message: "fill data first" });
+  }
+
+  const query = `UPDATE books SET ${fields.join(", ")} WHERE id = ?`;
+  values.push(id);
+  db.query(query, values, (err, data) => {
+    if (err) {
+      return res.status(500).json({ success: false, message: err });
+    }
+    if (data.affectedRows === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Book not found" });
+    }
+    return res
+      .status(200)
+      .json({ success: true, message: "Book updated successfully" });
+  });
+};
 
 export const deleteBook = (req, res) => {
   const { id } = req.params;
