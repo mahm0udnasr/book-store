f8f99ae0c20cf395e4a9ce5b1309561cbc3e7198
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-09-22 08:44:42 +0300

    add a addBook fn

--- controllers/booksControllers.js
+++ controllers/booksControllers.js
@@ -4,8 +4,29 @@ export const getBooks = (req, res) => {
   const query = "SELECT * FROM books";
   db.query(query, (err, data) => {
     if (err) {
-      return res.status(404).json({ success: false, message: err });
+      return res.status(500).json({ success: false, message: err });
     }
     return res.status(200).json({ success: true, data });
+  });
+};
+
+export const addBook = (req, res) => {
+  const { title, description, cover } = req.body;
+  if (!title || !description || !cover) {
+    return res
+      .status(400)
+      .json({ success: false, message: "fill all data first" });
+  }
+  const query = "INSERT INTO books (`title`,`description`,`cover`) VALUES (?)";
+  const values = [title, description, cover];
+  db.query(query, [values], (err, data) => {
+    if (err) {
+      return res.status(500).json({ success: false, message: err });
+    }
+    return res.status(201).json({
+      success: true,
+      message: "Book added successfully",
+      bookId: data.insertId,
+    });
   });
 };
