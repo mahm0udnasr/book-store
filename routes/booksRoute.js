25177ceb4aaf1e32fc0c17bf9f878afb1c8ba137
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-09-22 08:53:43 +0300

    add deleted book route

--- routes/booksRoute.js
+++ routes/booksRoute.js
@@ -2,9 +2,15 @@ import express from "express";
 const router = express.Router();
 
 // controller functions
-import { getBooks, addBook } from "../controllers/booksControllers.js";
+import {
+  getBooks,
+  addBook,
+  deleteBook,
+} from "../controllers/booksControllers.js";
 
 router.get("/", getBooks);
 router.post("/", addBook);
+
+router.delete("/:id", deleteBook);
 
 export default router;
