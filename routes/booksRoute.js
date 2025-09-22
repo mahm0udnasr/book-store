445bc5e0d93da08dae6935ba54cd09b7cc8c36ce
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-09-22 09:06:58 +0300

    add update route with PATCH method

--- routes/booksRoute.js
+++ routes/booksRoute.js
@@ -6,11 +6,12 @@ import {
   getBooks,
   addBook,
   deleteBook,
+  updateBook,
 } from "../controllers/booksControllers.js";
 
 router.get("/", getBooks);
 router.post("/", addBook);
-
+router.patch("/:id", updateBook);
 router.delete("/:id", deleteBook);
 
 export default router;
